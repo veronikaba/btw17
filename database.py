9f69b2288be16417ebba8993e949ebcb1289cc3c
EKo Sup <e.supardjo@gmx.de>
2019-01-12 14:33:38 +0100

    filling database using csv WIP

--- database.py
+++ database.py
@@ -8,10 +8,16 @@ from sqlalchemy import Integer, Column, String
 from flask import Flask
 from flask import render_template
 from flask import g
+from numpy import genfromtxt
 import sqlite3
 
+def Load_Data(file_name):
+    data = genfromtxt(file_name, delimiter=',')
+    return data.tolist
 
+Base = declarative_base()
 engine = create_engine('sqlite:///btw17.db')
+Base.metadata.create_all(engine)
 Session = sessionmaker(bind = engine, autocommit = True)
 session = Session()
 
@@ -19,7 +25,8 @@ session = Session()
 app = Flask(__name__)
 
 
-Base = declarative_base()
+
+
 class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key = True, nullable = False)
@@ -28,6 +35,12 @@ class State(Base):
     number = Column(Integer)
     constituencies = relationship('Constituency')
 
+
+#constintuency_party = Table ('constituency_parties',
+#    Base.metadata,
+#   Column (constituency_id,Integer, ForeignKey('parties.id')),
+#   Column (party_id,Integer, ForeignKey('constituencies.id')))
+ 
 class Constituency(Base):
     __tablename__ = 'constituencies'    
     id = Column(Integer, primary_key = True, nullable = False)
@@ -35,6 +48,7 @@ class Constituency(Base):
     state_id = Column(Integer, ForeignKey('states.id'))
     state = relationship('State')
     parties = relationship('Party')
+    belongs_to = Column(Integer)
 
 class Party(Base):
     __tablename__ = 'parties'
@@ -47,21 +61,45 @@ class Party(Base):
 class Votes(Base):
     __tablename__ = 'votes'
     id = Column(Integer, primary_key=True)
-    provisionallyVotes = Column(Integer)
+    provisionally_votes = Column(Integer)
     party_id = Column(Integer, ForeignKey('parties.id'))
     party = relationship('Party')
 
-Base.metadata.create_all(engine)
+
+try:
+    file_name = 'btw17_kerg_modify.csv'
+    data = Load_Data(file_name)
+
+    for i in data:
+        constituency = Constituency(**{
+         'name' : i[1],
+         'number' : i[0],
+         'belongs_to' : i[2]
+        })
+        j = 3
+        if j % 4 == 0:
+            record = Party(**{
+                'name' : i[i+1],
+                'constituency_id' : constituency.id
+             })
+        session.add(record)
+        session.add(constituency)
+        session.commit()
+except:
+    session.rollback()
+
+finally:
+    session.close()
     
+
+
 def get_db () :
  if not hasattr(g,'sqlite_db'):
-  con = sqlite3.connect('btw17.db')#typo?
-
+  con = sqlite3.connect('btw17.db')
   g.sqlite_db = con
  return g.sqlite_db
 
 @app.teardown_appcontext
 def close_db (error):
   if hasattr (g, 'sqlite_db'):g.sqlite_db.close ()
-
 app.run(debug = True)
