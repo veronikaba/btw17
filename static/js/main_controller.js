5946ff917770f98cfa33470e1128038d291e2232
babic <Veronika.Babic@adesso.de>
2019-01-15 17:27:19 +0100

    show states and constituencies - WIP

--- static/js/main_controller.js
+++ static/js/main_controller.js
@@ -1,11 +1,38 @@
+var app = angular.module('app', []);
 (function () {
   // the array contains modules that this module
   // depends on. In this case: none => empty array
-  var app = angular.module('app', []);
 
   // constructor of the controller
   // variable $scope is the ViewModel
   app.controller("main_controller", function ($scope) {
-    $scope.all_names = ["A", "B", "c"];
+    $scope.states = [
+      "Baden-Wüttemberg",
+      "Bayern",
+      "Berlin",
+      "Brandenburg",
+      "Bremen",
+      "Hamburg",
+      "Hessen",
+      "Mecklenburg-Vorpommern",
+      "Niedersachsen",
+      "Nordreihn-Westfalen",
+      "Reihnland-Pfalz",
+      "Saarland",
+      "Sachsen",
+      "Sachsen-Anhalt",
+      "Schleswig-Holstein",
+      "Thüringen",
+    ];
+
+    $scope.showConstituencies = function () {
+      $scope.constituencies = [
+        "Berlin-Charlottenburg-Wilmersdorf",
+        "Berlin-Friedrichshein-Kreuzberg-Prenzlauerberg Ost",
+        "Berlin-Lichtenberg",
+        "Berlin-Marzah-Hellerdorf",
+      ];
+    }
   });
-})();
+
+})(app);
