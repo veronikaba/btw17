60652dca87d9cdd9bfea0a1fe66aebf055ef2fef
Veronika <veronika.babic@web.de>
2019-01-17 18:45:42 +0100

    added line and bar chart from angular-chart.js

--- static/js/main_controller.js
+++ static/js/main_controller.js
@@ -1,5 +1,4 @@
-var app = angular.module('app', []);
-(function () {
+var app = angular.module('app', ["chart.js"]);
   // the array contains modules that this module
   // depends on. In this case: none => empty array
 
@@ -34,8 +33,28 @@ var app = angular.module('app', []);
       ];
       /*constituencies.push($scope.test);*/
     }
-
-
   });
 
-})(app);
+  app.controller('bar_controller', function($scope){
+    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+  });
+
+  app.controller('firstVotes_pie_ctrl', function($scope){
+    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    $scope.data = [
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+  });
+
+  app.controller('secondVotes_pie_ctrl', function($scope){
+    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    $scope.data = [
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+  });
+
